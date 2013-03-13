2716d3eb5d37680396315de7378a12c051ef000d
Geoffrey Ulman <ulman@metsci.com>
2013-03-13 17:59:47 -0400

    Added classifier Class stub.

---
+++ weka/src/main/java/edu/gmu/vfml/classifier/VFML.java
@@ -0,0 +1,37 @@
+package edu.gmu.vfml.classifier;
+
+import weka.classifiers.Classifier;
+import weka.core.Instances;
+import weka.core.TechnicalInformation;
+import weka.core.TechnicalInformation.Field;
+import weka.core.TechnicalInformation.Type;
+import weka.core.TechnicalInformationHandler;
+
+public class VFML extends Classifier implements TechnicalInformationHandler
+{
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public void buildClassifier( Instances instances ) throws Exception
+    {
+        // TODO Auto-generated method stub
+    }
+
+    public TechnicalInformation getTechnicalInformation( )
+    {
+        TechnicalInformation info = new TechnicalInformation( Type.ARTICLE );
+        
+        info.setValue( Field.AUTHOR, "Domingos, Pedro" );
+        info.setValue( Field.YEAR, "2000" );
+        info.setValue( Field.TITLE, "Mining high-speed data streams" );
+        info.setValue( Field.JOURNAL, "Proceedings of the sixth ACM SIGKDD international conference on Knowledge discovery and data mining" );
+        info.setValue( Field.SERIES, "KDD '00" );
+        info.setValue( Field.ISBN, "1-58113-233-6" );
+        info.setValue( Field.LOCATION, "Boston, Massachusetts, USA" );
+        info.setValue( Field.PAGES, "71-80" );
+        info.setValue( Field.URL, "http://doi.acm.org/10.1145/347090.347107" );
+        info.setValue( Field.PUBLISHER, "ACM" );
+        
+        return info;
+    }
+}
