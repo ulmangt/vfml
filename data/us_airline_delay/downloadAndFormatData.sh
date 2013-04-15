f1de385c25ec7b065a5bf90394590a8a850bf676
Geoffrey Ulman <ulman@metsci.com>
2013-04-14 22:06:50 -0300

    Update downloadAndFormatData.sh

--- data/us_airline_delay/downloadAndFormatData.sh
+++ data/us_airline_delay/downloadAndFormatData.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # download the airline data set
-download_data.sh
+./download_data.sh
 
 # run data formatting python script to make one large arff formatted file
 python formatData.py . data header
